127de689da9eaead47e6dbf9ff72d70826b3c781
Yann Bourdeau <yann.bourdeau@mac.com>
2025-05-09 17:38:44 -0400

    dernière modification

--- src/darwin/AppDelegate.mm
+++ src/darwin/AppDelegate.mm
@@ -15,7 +15,7 @@
     [panel setTitle:@"Ajouter un projet"];
     [panel center];
     
-    // Add some controls to the panel
+    // Add some controls to the panel - with improved alignment and sizing
     NSTextField *nameLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(20, 250, 100, 20)];
     [nameLabel setStringValue:@"Nom:"];
     [nameLabel setBezeled:NO];
@@ -32,13 +32,32 @@
     [descLabel setEditable:NO];
     [descLabel setSelectable:NO];
     
-    NSTextField *descField = [[NSTextField alloc] initWithFrame:NSMakeRect(120, 220, 250, 60)];
+    NSTextField *descField = [[NSTextField alloc] initWithFrame:NSMakeRect(120, 220, 250, 20)];
+    
+    // Create a dictionary to store the form fields for access in the OK button handler
+    NSMutableDictionary *formFields = [NSMutableDictionary dictionary];
+    [formFields setObject:nameField forKey:@"nameField"];
+    [formFields setObject:descField forKey:@"descField"];
     
     NSButton *okButton = [[NSButton alloc] initWithFrame:NSMakeRect(270, 20, 100, 30)];
     [okButton setTitle:@"OK"];
     [okButton setBezelStyle:NSBezelStyleRounded];
-    [okButton setAction:@selector(closePanel:)];
-    [okButton setTarget:panel];
+    
+    // Set up a custom selector for the OK button that will process the form data
+    [okButton setAction:@selector(handleOKButton:)];
+    
+    // Store the form fields dictionary in the button's represented object
+    [okButton setRepresentedObject:formFields];
+    
+    // Set the target to self (AppDelegate)
+    [okButton setTarget:self];
+    
+    // Add a cancel button
+    NSButton *cancelButton = [[NSButton alloc] initWithFrame:NSMakeRect(160, 20, 100, 30)];
+    [cancelButton setTitle:@"Cancel"];
+    [cancelButton setBezelStyle:NSBezelStyleRounded];
+    [cancelButton setAction:@selector(handleCancelButton:)];
+    [cancelButton setTarget:self];
     
     // Add controls to panel
     [[panel contentView] addSubview:nameLabel];
@@ -46,9 +65,93 @@
     [[panel contentView] addSubview:descLabel];
     [[panel contentView] addSubview:descField];
     [[panel contentView] addSubview:okButton];
+    [[panel contentView] addSubview:cancelButton];
+    
+    // Store the panel in the button's represented object for access in the handler
+    [formFields setObject:panel forKey:@"panel"];
+    
+    // Set the panel to close when the escape key is pressed
+    [panel setDefaultButtonCell:[okButton cell]];
+    
+    // Also handle window close button
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(windowWillClose:)
+                                                 name:NSWindowWillCloseNotification
+                                               object:panel];
     
     // Show the panel
     [NSApp runModalForWindow:panel];
+    
+    // Release the panel - it will be closed by the button handlers
+    [panel release];
+}
+
+// Handler for the OK button in the Ajouter panel
+- (void)handleOKButton:(id)sender {
+    // Get the form fields from the button's represented object
+    NSDictionary *formFields = [sender representedObject];
+    NSTextField *nameField = [formFields objectForKey:@"nameField"];
+    NSTextField *descField = [formFields objectForKey:@"descField"];
+    NSPanel *panel = [formFields objectForKey:@"panel"];
+    
+    // Get the values from the form fields
+    NSString *name = [nameField stringValue];
+    NSString *description = [descField stringValue];
+    
+    // Validate the form data
+    if ([name length] == 0) {
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert setMessageText:@"Error"];
+        [alert setInformativeText:@"Name is required"];
+        [alert addButtonWithTitle:@"OK"];
+        [alert runModal];
+        [alert release];
+        return;
+    }
+    
+    // Log the form data
+    NSLog(@"Project Name: %@", name);
+    NSLog(@"Project Description: %@", description);
+    
+    // TODO: Add code to save the project to the database
+    // Based on the codebase context, you would use zrestdb::Project to create a new project
+    
+    // Stop the modal session and close the panel
+    [NSApp stopModal];
+    
+    // Remove the observer to prevent memory leaks
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:NSWindowWillCloseNotification
+                                                  object:panel];
+    
+    [panel orderOut:nil];
+}
+
+// Handler for the Cancel button
+- (void)handleCancelButton:(id)sender {
+    // Find the panel (it's the window that contains the button)
+    NSWindow *panel = [sender window];
+    
+    // Stop the modal session and close the panel
+    [NSApp stopModal];
+    
+    // Remove the observer to prevent memory leaks
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:NSWindowWillCloseNotification
+                                                  object:panel];
+    
+    [panel orderOut:nil];
+}
+
+// Handler for window close notification
+- (void)windowWillClose:(NSNotification *)notification {
+    // Stop the modal session when the window is closed
+    [NSApp stopModal];
+    
+    // Remove the observer to prevent memory leaks
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:NSWindowWillCloseNotification
+                                                  object:[notification object]];
 }
 
 // Called when the application is activated (including dock icon click)
@@ -76,6 +179,8 @@
 }
 
 - (void)dealloc {
+    // Make sure we remove any observers
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     [mainWindow release];
     [super dealloc];
 }
--- src/darwin/main_darwin.mm
+++ src/darwin/main_darwin.mm
@@ -34,8 +34,8 @@ int zrestgui::main_app(int argc, char *argv[])
     [appMenu addItem:quitMenuItem];
     [appMenuItem setSubmenu:appMenu];
     
-    // Create the main window
-    NSWindow *window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 200)
+    // Create the main window with larger dimensions to accommodate the list box
+    NSWindow *window = [[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 800, 600)
         styleMask:NSWindowStyleMaskClosable|NSWindowStyleMaskResizable|NSWindowStyleMaskTitled 
         backing:NSBackingStoreBuffered defer:NO] autorelease];
     
@@ -48,21 +48,58 @@ int zrestgui::main_app(int argc, char *argv[])
     //[window setContentViewController:vc];           
     [window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
     [window setTitle:appName];
-    [window makeKeyAndOrderFront:window];
-
-    NSButton *buttonQuit = [[NSButton alloc] initWithFrame:NSMakeRect(700,150, 100, 20)];
+    
+    // Create a scroll view to contain the table view (list box)
+    NSScrollView *scrollView = [[NSScrollView alloc] initWithFrame:NSMakeRect(5, 5, 790, 550)];
+    [scrollView setBorderType:NSBezelBorder];
+    [scrollView setHasVerticalScroller:YES];
+    [scrollView setHasHorizontalScroller:YES];
+    [scrollView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    
+    // Create a table view (macOS equivalent of a list box)
+    NSTableView *tableView = [[NSTableView alloc] initWithFrame:NSMakeRect(0, 0, 790, 550)];
+    
+    // Add a column to the table view
+    NSTableColumn *column = [[NSTableColumn alloc] initWithIdentifier:@"name"];
+    [column setWidth:780];
+    [column setEditable:NO];
+    [column setHeaderCell:[[NSTableHeaderCell alloc] initTextCell:@"Projects"]];
+    [tableView addTableColumn:column];
+    [column release];
+    
+    // Configure the table view
+    [tableView setHeaderView:[[NSTableHeaderView alloc] init]];
+    [tableView setAllowsMultipleSelection:NO];
+    [tableView setAllowsColumnReordering:NO];
+    [tableView setAllowsColumnResizing:YES];
+    [tableView setAllowsEmptySelection:YES];
+    [tableView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    
+    // Set the table view as the document view of the scroll view
+    [scrollView setDocumentView:tableView];
+    [tableView release];
+    
+    // Add the scroll view to the window
+    [[window contentView] addSubview:scrollView];
+    [scrollView release];
+    
+    // Add buttons at the bottom of the window
+    NSButton *buttonQuit = [[NSButton alloc] initWithFrame:NSMakeRect(700, 560, 100, 30)];
     [buttonQuit setTitle:@"Quitter"];
     [buttonQuit setBezelStyle:NSBezelStyleRoundRect];
     [buttonQuit setAction:@selector(terminate:)];
     [[window contentView] addSubview: buttonQuit];
+    [buttonQuit release];
 
-    NSButton *buttonAjout = [[NSButton alloc] initWithFrame:NSMakeRect(700, 170, 100, 20)];
+    NSButton *buttonAjout = [[NSButton alloc] initWithFrame:NSMakeRect(590, 560, 100, 30)];
     [buttonAjout setTitle:@"Ajouter"];
     [buttonAjout setBezelStyle:NSBezelStyleRoundRect];
     [buttonAjout setAction:@selector(ajouter:)];
     [buttonAjout setTarget:appDelegate];  // Set the target to our app delegate
     [[window contentView] addSubview: buttonAjout];
+    [buttonAjout release];
     
+    [window makeKeyAndOrderFront:window];
     [NSApp activateIgnoringOtherApps:YES];
     [NSApp run];
     [appDelegate release];  // Release our app delegate
